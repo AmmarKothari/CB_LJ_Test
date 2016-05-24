ec9f291fdf3499409720f822bf762cab07399657
Ammar Kothari <akothari@rethinkrobotics.com>
2016-05-24 11:01:54 -0400

    Updated resistor values and calculation of voltages

--- CB_LJ_Test.py
+++ CB_LJ_Test.py
@@ -13,13 +13,19 @@ def init_setup():
     SevenFivePin = 0 #AI0
     TwentyFourPin = 1 #AI1
     print("Pins Set")
+    #####LOG TO FILE#####
+    log_file_name = ""
+    #logging.basicConfigicConfig(filename=log_file_name, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+    ####send to terminal####
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+    
     #########RESISTORS###############
-    R_TF_1 = 1000
-    R_TF_2 = 1000
-    R_S_1 = 1000
-    R_S_1 = 1000
-    R_LJ = 46000
-    REff = lambda R_1, R_2: return 1/(1/R_1 + 1/R_2)
+    R_TF_1 = float(1100)
+    R_TF_2 = float(360)
+    R_S_1 = float(1100)
+    R_S_2 = float(1100)
+    R_LJ = float(46000)
+    REff = lambda R_1, R_2: 1/(1/R_1 + 1/R_2)
     R_TF_E = REff(R_LJ, R_TF_2)
     R_S_E = REff(R_LJ, R_S_2)
     global R_TF_Ratio, R_S_Ratio
@@ -27,24 +33,19 @@ def init_setup():
     R_S_Ratio = R_S_E/(R_S_1 + R_S_E)
     #########GENERAL CONSTANTS#########
     num_readings = 10
-    #####LOG TO FILE#####
-    log_file_name = ""
-    #logging.basicConfigicConfig(filename=log_file_name, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-    ####send to terminal####
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
     ########Initializing Lab Jack#######
     try:
         LJ1=u12.U12() #labjack object
-        print(LJ1)
+        # print(LJ1)
         print("Success!")
     except Exception, e:
         print("THIS IS NOT WORKING!")
         print(e)
         sys.exit()
         #LJ1.open()
-    LJ1_ID = LJ1.devid() #labjack id should be zero
+    LJ1_ID = LJ1.id #labjack id should be zero
     if LJ1_ID != 0:
-    	#    print("There may be multiple Lab Jacks Connected.  BEWARE!")
+    	print("There may be multiple Lab Jacks Connected.  BEWARE!")
     return LJ1_ID 
     #verify serial number?
     #verify only one lab jack connected
@@ -71,16 +72,20 @@ def Reset_LabJack():
 def SevenFiveVCheck():
     global SevenFivePin, R_S_Ratio
     logging.debug("7.5V Voltage Check")
-    AverageVoltage = MeasureVoltage(SevenFivePin) / R_S_Ratio #Voltage read with initialized divider. Adjust to find CB output.
-    Result = VoltageAcceptable(AverageVoltage, 7, 8)
-    return(AverageVoltage, Result)
+    AverageReadVoltage = MeasureVoltage(SevenFivePin) #measure actual voltage through divider
+    AverageAdjustedVoltage = AverageReadVoltage / R_S_Ratio #Adjust by divider to find CB output.
+    Result = VoltageAcceptable(AverageAdjustedVoltage, 7, 8) #Compare to acceptable values
+    logging.debug("Adjusted Volgate: %s" %AverageAdjustedVoltage)
+    return(AverageAdjustedVoltage, Result)
 
 def TwentyFourLowVCheck():
     global TwentyFourPin, R_TF_Ratio
     logging.debug("24V Low Voltage Check")
-    AverageVoltage = MeasureVoltage(TwentyFourPin) / R_TF_Ratio #Voltage read with initialized divider. Adjust to find CB output.
-    Result = VoltageAcceptable(AverageVoltage, -.1, .1)
-    return(AverageVoltage, Result)
+    AverageReadVoltage = MeasureVoltage(TwentyFourPin)
+    AverageAdjustedVoltage = AverageReadVoltage / R_TF_Ratio #Voltage read with initialized divider. Adjust to find CB output.
+    Result = VoltageAcceptable(AverageAdjustedVoltage, -.1, .1)
+    logging.debug("Adjusted Volgate: %s" %AverageAdjustedVoltage)
+    return(AverageAdjustedVoltage, Result)
 
 def VoltageCheck(input_pin, avg_V, LL_V, UL_V):
     AverageVoltage = MeasureVoltage(input_pin)
@@ -110,6 +115,7 @@ def MeasureVoltage(pin_number):
     print("Average Voltage: %s" %AverageVoltage)
     StdVoltage = numpy.std(VoltageReadings)
     print("Standard Deviation of Voltage: %s" %StdVoltage)
+    return AverageVoltage
 
 def test_Sequence():
     global LJ1
@@ -118,6 +124,7 @@ def test_Sequence():
     #Assuming only 1 lab jack is connected
     #Reset labjack
     #Reset_LabJack()
+    #is a calibration check necessary???
     #Check for 7.5V
     SevenFiveVCheck()
     #Check for 8V (24 low voltage)
